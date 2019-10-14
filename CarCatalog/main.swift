f2865fcaf9dd0c936b5a64cf0a7eaedd4e036cf8
Fedor Zverev <f.zv@ya.ru>
2019-10-14 21:13:28 +0300

    Initial Commit

---
+++ CarCatalog/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CarCatalog
+//
+//  Created by Fedor Zverev on 14.10.2019.
+//  Copyright © 2019 Fedor Zverev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
