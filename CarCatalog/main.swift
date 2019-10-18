aa21335fda3afc2da69230371462dec1c3ebf890
Fedor Zverev <f.zv@ya.ru>
2019-10-19 00:03:56 +0300

    2

--- CarCatalog/main.swift
+++ CarCatalog/main.swift
@@ -1,7 +1,6 @@
 //  main.swift
 //  CarCatalog
-//
-//  Created by Fedor Zverev on 14.10.2019.
+//  Created by Fedor Zverev on 14.10.2019.
 //  Copyright © 2019 Fedor Zverev. All rights reserved.
 
 import Foundation
